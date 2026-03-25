339e4b248c0b9730e1d70421a70f2f077581e50c
Yuvasri Murugan <yuvasrims01@gmail.com>
2026-03-25 15:49:49 +0530

    Delete server directory

--- server/config/mongodb.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-const connectDB = async () =>{
-    mongoose.connection.on('connected',()=>{
-        console.log("Database Connected")
-    })
-    await mongoose.connect(`${process.env.MONGODB_URI}/client\vite-project`)
-}
-export default connectDB;
