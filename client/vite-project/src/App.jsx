c166b5781eff9adb65ab292a0f0c0e8972c45fb7
Yuvasri Murugan <yuvasrims01@gmail.com>
2026-03-25 15:39:54 +0530

    fix tailwind typo

--- client/vite-project/src/App.jsx
+++ client/vite-project/src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
 
     return (
         <div className='px-4 sm:px-10 md:px-14 lg:px-28 
-        min-h-screen bg-gradient-to-b frm-teal-50 
+        min-h-screen bg-gradient-to-b from-teal-50 
         to-orange-50'>
 
             <ToastContainer position='bottom-right'/>
